7fa530cd00b0f665d46369338ee5d9efdcf2cd0f
henk-vd-brink <hcebrink@gmail.com>
2021-12-26 20:08:46 +0100

    WORKS!

--- api/src/main.cpp
+++ api/src/main.cpp
@@ -1,17 +1,20 @@
+#include <iostream>
 #include <served/served.hpp>
 
-int main(int argc, char const *argv[])
+int main(int argc, const char *argv[])
 {
-    // Create a multiplexer for handling requests
     served::multiplexer mux;
+    mux.handle("/api/v1/greeting")
+        .get([&](served::response &res, const served::request &req)
+             {
+            std::string name = req.query["name"];
+            res.set_header("content-type", "application/json");
+            res << "{ \"content\": \"Hello, " << ((name.length() > 0) ? name : "world") << "!\" }\n"; });
 
-    // GET /hello
-    mux.handle("/hello")
-        .get([](served::response &res, const served::request &req)
-             { res << "Hello world!"; });
+    std::cout << "Try this example with:" << std::endl;
+    std::cout << "  curl \"http://localhost:8123/api/v1/greeting?name=world\"" << std::endl;
 
-    // Create the server and run with 10 handler threads.
-    served::net::server server("127.0.0.1", "8081", mux);
+    served::net::server server("0.0.0.0", "8123", mux);
     server.run(10);
 
     return (EXIT_SUCCESS);
